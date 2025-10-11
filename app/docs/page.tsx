377e02f5aa29122c33c0650fbb160d950b8f147c
kay <kay@kayui-MacBookPro.local>
2025-10-11 16:28:49 +0900

    DOCS: Docs 메인 글 수정

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,17 +1,19 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
-import { allProjects } from 'contentlayer/generated';
-import { Navigation } from '../components/nav';
-import { Card } from '../components/card';
-import { Article } from './article';
+import { allProjects } from "contentlayer/generated";
+import { Navigation } from "../components/nav";
+import { Card } from "../components/card";
+import { Article } from "./article";
 
 export default async function DocsPage() {
   const featured = allProjects.find(
-    (project) => project.slug === 'apply-articlejsonId'
+    (project) => project.slug === "payment-system-modularization"
   )!;
-  const top2 = allProjects.find((project) => project.slug === 'forms')!;
-  const top3 = allProjects.find((project) => project.slug === 'babel')!;
+  const top2 = allProjects.find((project) => project.slug === "waiting-room")!;
+  const top3 = allProjects.find(
+    (project) => project.slug === "library-customize"
+  )!;
   const sorted = allProjects
     .filter((p) => p.published)
     .filter(
@@ -27,29 +29,29 @@ export default async function DocsPage() {
     );
 
   return (
-    <div className='relative pb-16'>
+    <div className="relative pb-16">
       <Navigation />
-      <div className='px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32'>
-        <div className='max-w-2xl mx-auto lg:mx-0'>
-          <h2 className='text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl'>
+      <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+        <div className="max-w-2xl mx-auto lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
             Docs
           </h2>
-          <p className='mt-4 text-zinc-400'>
+          <p className="mt-4 text-zinc-400">
             These are a summary of what I studied.
           </p>
         </div>
-        <div className='w-full h-px bg-zinc-800' />
+        <div className="w-full h-px bg-zinc-800" />
 
-        <div className='grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 '>
+        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
           <Card>
             <Link href={`/docs/${featured.slug}`}>
-              <article className='relative w-full h-full p-4 md:p-8'>
-                <div className='flex items-center justify-between gap-2'>
-                  <div className='text-xs text-zinc-100'>
+              <article className="relative w-full h-full p-4 md:p-8">
+                <div className="flex items-center justify-between gap-2">
+                  <div className="text-xs text-zinc-100">
                     {featured.date ? (
                       <time dateTime={new Date(featured.date).toISOString()}>
                         {Intl.DateTimeFormat(undefined, {
-                          dateStyle: 'medium',
+                          dateStyle: "medium",
                         }).format(new Date(featured.date))}
                       </time>
                     ) : (
@@ -59,24 +61,24 @@ export default async function DocsPage() {
                 </div>
 
                 <h2
-                  id='featured-post'
-                  className='mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display'
+                  id="featured-post"
+                  className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
                 >
                   {featured.title}
                 </h2>
-                <p className='mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300'>
+                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
                   {featured.description}
                 </p>
-                <div className='absolute bottom-4 md:bottom-8'>
-                  <p className='hidden text-zinc-200 hover:text-zinc-50 lg:block'>
-                    Read more <span aria-hidden='true'>&rarr;</span>
+                <div className="absolute bottom-4 md:bottom-8">
+                  <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
+                    Read more <span aria-hidden="true">&rarr;</span>
                   </p>
                 </div>
               </article>
             </Link>
           </Card>
 
-          <div className='flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 '>
+          <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
             {[top2, top3].map((project) => (
               <Card key={project.slug}>
                 <Article project={project} />
@@ -85,10 +87,10 @@ export default async function DocsPage() {
           </div>
         </div>
 
-        <div className='hidden w-full h-px md:block bg-zinc-800' />
+        <div className="hidden w-full h-px md:block bg-zinc-800" />
 
-        <div className='grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3'>
-          <div className='grid grid-cols-1 gap-4'>
+        <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
+          <div className="grid grid-cols-1 gap-4">
             {sorted
               .filter((_, i) => i % 3 === 0)
               .map((project) => (
@@ -97,7 +99,7 @@ export default async function DocsPage() {
                 </Card>
               ))}
           </div>
-          <div className='grid grid-cols-1 gap-4'>
+          <div className="grid grid-cols-1 gap-4">
             {sorted
               .filter((_, i) => i % 3 === 1)
               .map((project) => (
@@ -106,7 +108,7 @@ export default async function DocsPage() {
                 </Card>
               ))}
           </div>
-          <div className='grid grid-cols-1 gap-4'>
+          <div className="grid grid-cols-1 gap-4">
             {sorted
               .filter((_, i) => i % 3 === 2)
               .map((project) => (
